a24ee9071dbd8002126fc4fe73a42ce7883588bb
dubuqingfeng <1135326346@qq.com>
2014-06-22 01:23:23 +0800

    更新数据表，将节点，角色等数据表加入。

--- fanheo.sql
+++ fanheo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2014-06-21 13:12:48
+-- Generation Time: 2014-06-21 19:22:51
 -- 服务器版本： 5.6.17
 -- PHP Version: 5.5.12
 
@@ -19,6 +19,21 @@ SET time_zone = "+00:00";
 --
 -- Database: `fanheo`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `fanheo_access`
+--
+
+CREATE TABLE IF NOT EXISTS `fanheo_access` (
+  `role_id` smallint(6) unsigned NOT NULL,
+  `node_id` smallint(6) unsigned NOT NULL,
+  `level` tinyint(1) NOT NULL,
+  `module` varchar(50) DEFAULT NULL,
+  KEY `groupId` (`role_id`),
+  KEY `nodeId` (`node_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -49,7 +64,79 @@ CREATE TABLE IF NOT EXISTS `fanheo_admin` (
 --
 
 INSERT INTO `fanheo_admin` (`uid`, `username`, `password`, `roletype`, `mobile`, `phone`, `lastip`, `email`, `sex`, `lastlogintime`, `truename`, `num`, `loginip`, `logintime`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, 13500000000, NULL, '127.0.0.1', NULL, NULL, 1403348710, NULL, 10, '127.0.0.1', 1403348848);
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, 13500000000, NULL, '127.0.0.1', NULL, NULL, 1403365834, NULL, 15, '127.0.0.1', 1403367394);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `fanheo_node`
+--
+
+CREATE TABLE IF NOT EXISTS `fanheo_node` (
+  `id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '0',
+  `remark` varchar(255) DEFAULT NULL,
+  `sort` smallint(6) unsigned DEFAULT NULL,
+  `pid` smallint(6) unsigned NOT NULL,
+  `level` tinyint(1) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `level` (`level`),
+  KEY `pid` (`pid`),
+  KEY `status` (`status`),
+  KEY `name` (`name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- 转存表中的数据 `fanheo_node`
+--
+
+INSERT INTO `fanheo_node` (`id`, `name`, `title`, `status`, `remark`, `sort`, `pid`, `level`) VALUES
+(1, 'admin', '后台应用', 1, NULL, 1, 0, 1),
+(2, 'a', '前台应用', 1, NULL, 1, 0, 1),
+(3, 'a', '前台应用', 1, NULL, 1, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `fanheo_role`
+--
+
+CREATE TABLE IF NOT EXISTS `fanheo_role` (
+  `id` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `pid` smallint(6) DEFAULT NULL,
+  `status` tinyint(1) unsigned DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pid` (`pid`),
+  KEY `status` (`status`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- 转存表中的数据 `fanheo_role`
+--
+
+INSERT INTO `fanheo_role` (`id`, `name`, `pid`, `status`, `remark`) VALUES
+(1, 'admin', NULL, 1, '超级管理员'),
+(2, 'productmanager', NULL, 1, '产品管理员'),
+(3, 'Logisticmanage', NULL, 1, '物流管理员'),
+(4, 'Logisticmanage', NULL, 1, '物流管理员'),
+(5, 'shop', NULL, 1, '订单统计人员');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `fanheo_role_user`
+--
+
+CREATE TABLE IF NOT EXISTS `fanheo_role_user` (
+  `role_id` mediumint(9) unsigned DEFAULT NULL,
+  `user_id` char(32) DEFAULT NULL,
+  KEY `group_id` (`role_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
