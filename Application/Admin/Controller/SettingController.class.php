f757687dd1bef1e08b9563ce3d7a03ab6a3ea420
dubuqingfeng <1135326346@qq.com>
2014-07-14 22:24:04 +1000

    update，那个叫做admin_sidebar的孩子，以及添加了那个叫做phpmailer的库子

---
+++ Application/Admin/Controller/SettingController.class.php
@@ -0,0 +1,34 @@
+<?php
+/**  
+ * @author 
+ * @author 
+ */
+namespace Admin\Controller;
+use Think\Controller;
+
+class SettingController extends Controller{
+	//邮箱管理
+	public function email(){
+        $email = M('smtp');
+        $list = $email->select();
+        $this->assign('vo',$list);
+        $this->display();
+    }
+    //邮箱保存
+    public function email_send(){
+            $system=D('Smtp');
+			if($system->create()){
+				  $result = $system->save();
+				 if($result){
+					 $this->Record('SMTP修改成功');//后台操作
+					 $this->success('修改成功', '__URL__/email');
+					
+				 }else{
+					 $this->Record('SMTP修改失败');//后台操作
+					$this->error("修改失败");
+				 }			 			
+			}else{
+				 $this->error($system->getError());
+			}
+    }
+   }
